4fe59bf057657310531390d202d715155df2c8d3
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2024-09-04 15:13:57 +0200

    Correcciones en cli y cambio forma de usar filtros

--- src/Filter/LoginFilter.php
+++ src/Filter/LoginFilter.php
@@ -4,28 +4,30 @@ namespace Osumi\OsumiFramework\App\Filter;
 
 //use Osumi\OsumiFramework\Plugins\OToken;
 
-/**
- * Security filter for clients
- *
- * @param array $params Parameter array received on the call
- *
- * @param array $headers HTTP header array received on the call
- *
- * @return array Return filter status (ok / error) and information
- */
-function LoginFilter(array $params, array $headers): array {
-	global $core;
-	/*
-	// Sample header authentication code
-	$ret = ['status'=>'error', 'id'=>null];
+class LoginFilter {
+	/**
+	 * Security filter for clients
+	 *
+	 * @param array $params Parameter array received on the call
+	 *
+	 * @param array $headers HTTP header array received on the call
+	 *
+	 * @return array Return filter status (ok / error) and information
+	 */
+	public static function handle(array $params, array $headers): array {
+		/*global $core;
 
-	$tk = new OToken($core->config->getExtra('secret'));
-	if ($tk->checkToken($headers['Authorization'])) {
-		$ret['status'] = 'ok';
-		$ret['id'] = intval($tk->getParam('id'));
+		// Sample header authentication code
+		$ret = ['status'=>'error', 'id'=>null];
+
+		$tk = new OToken($core->config->getExtra('secret'));
+		if ($tk->checkToken($headers['Authorization'])) {
+			$ret['status'] = 'ok';
+			$ret['id'] = intval($tk->getParam('id'));
+		}
+		*/
+		$ret = ['status'=>'ok', 'id'=>1];
+
+		return $ret;
 	}
-	*/
-	$ret = ['status'=>'ok', 'id'=>1];
-
-	return $ret;
 }
--- src/Filter/UserFilter.php
+++ src/Filter/UserFilter.php
@@ -4,28 +4,30 @@ namespace Osumi\OsumiFramework\App\Filter;
 
 //use Osumi\OsumiFramework\Plugins\OToken;
 
-/**
- * Gets user from the token
- *
- * @param array $params Parameter array received on the call
- *
- * @param array $headers HTTP header array received on the call
- *
- * @return array Return filter status (ok / error) and information
- */
-function UserFilter(array $params, array $headers): array {
-	global $core;
-	/*
-	// Sample header authentication code
-	$ret = ['status'=>'error', 'name'=>null];
+class UserFilter {
+	/**
+	 * Gets user from the token
+	 *
+	 * @param array $params Parameter array received on the call
+	 *
+	 * @param array $headers HTTP header array received on the call
+	 *
+	 * @return array Return filter status (ok / error) and information
+	 */
+	public static function handle(array $params, array $headers): array {
+		/*global $core;
 
-	$tk = new OToken($core->config->getExtra('secret'));
-	if ($tk->checkToken($headers['Authorization'])) {
-		$ret['status'] = 'ok';
-		$ret['id'] = intval($tk->getParam('id'));
+		// Sample header authentication code
+		$ret = ['status'=>'error', 'name'=>null];
+
+		$tk = new OToken($core->config->getExtra('secret'));
+		if ($tk->checkToken($headers['Authorization'])) {
+			$ret['status'] = 'ok';
+			$ret['id'] = intval($tk->getParam('id'));
+		}
+		*/
+		$ret = ['status' => 'ok', 'name' => 'User name'];
+
+		return $ret;
 	}
-	*/
-	$ret = ['status' => 'ok', 'name' => 'User name'];
-
-	return $ret;
 }
